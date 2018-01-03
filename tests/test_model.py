8b1f9a48b699cd61215edd7b1ef6a647cfeec4f3
unknown <profile2697@gmail.com>
2018-01-04 03:15:20 +0900

    [models] add tests of generator and feature extractor

---
+++ tests/test_model.py
@@ -0,0 +1,43 @@
+import unittest
+import tensorflow as tf
+
+from model import DTN
+
+
+class MockConfig:
+    def __init__(self, mode='pretrain', alpha=15, beta=15, gamma=0):
+        self.mode = mode
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+
+
+class TestDTN(unittest.TestCase):
+    def setUp(self):
+        tf.reset_default_graph()
+        self.model = DTN(tf.placeholder(tf.float32, shape=[None, 32, 32, 3], name='s_images'),
+                         tf.placeholder(tf.float32, shape=[None, 32, 32, 1], name='t_images'),
+                         tf.placeholder(tf.float32, name='learning_rate'),
+                         MockConfig())
+
+    def test_pretrain모드일_경우_feature_extractor는_logits를_리턴(self):
+        src_images = tf.placeholder(tf.float32, shape=[None, 32, 32, 3])
+        logits = self.model.feature_extractor(src_images)
+
+        self.assertListEqual([None, 10], logits.get_shape().as_list(),
+                             'Incorrect Image Shape.  Found {} shape'.format(logits.get_shape().as_list()))
+
+    def test_pretrain모드가_아닐경우_feature_extractor는_1x1x128형태의_activations반환(self):
+        self.model.mode = 'train'
+        src_images = tf.placeholder(tf.float32, shape=[None, 32, 32, 3])
+        activations = self.model.feature_extractor(src_images)
+
+        self.assertListEqual([None, 1, 1, 128], activations.get_shape().as_list(),
+                             'Incorrect Image Shape.  Found {} shape'.format(activations.get_shape().as_list()))
+
+    def test_generator는_1x1x128형태의_입력을_받아_32x32x1형태의_이미지를_생성(self):
+        fx = tf.placeholder(tf.float32, shape=[None, 1, 1, 128])
+        generated = self.model.generator(fx)
+
+        self.assertListEqual([None, 32, 32, 1], generated.get_shape().as_list(),
+                             'Incorrect Image Shape.  Found {} shape'.format(generated.get_shape().as_list()))
